86ab347a59d3a2efba08d33cdb2229804d74e14e
24kmystique <itsliwens@gmail.com>
2022-11-24 18:01:34 +0800

    added reviews and interview content in book page

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -14,11 +14,15 @@ class Book extends Component {
                 </div>
                 <div className='body-col-right'>
                     <h2>Of the Florids</h2>
-                    <p className='text'>Diode Editions, 2022</p>
-                    <p className='text'>Winner of the 2021 Diode Editions Chapbook Prize</p>
-                    <p className='text'>Runner-up of the 2021 Tupelo Press Sunken Garden Chapbook Prize</p>
-                    <p className='text description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus dui nec dignissim tempor. Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris. Sed sed euismod mi, sit amet hendrerit ipsum.</p>
+                    <p className='main-text'>Diode Editions, 2022</p>
+                    <p className='main-text'>Winner of the 2021 Diode Editions Chapbook Prize</p>
+                    <p className='main-text'>Runner-up of the 2021 Tupelo Press Sunken Garden Chapbook Prize</p>
+                    <p className='main-text description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus dui nec dignissim tempor. Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris. Sed sed euismod mi, sit amet hendrerit ipsum.</p>
                     <button>BUY NOW</button>
+                    <h3>Reviews</h3>
+                    <p className='text'><span>“Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris.” —</span><span className='reviews-signoff-name'>John Doe</span></p>
+                    <p className='text'><span>“Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris.” —</span><span className='reviews-signoff-name'>John Doe</span></p>
+                    <h3>Interviews</h3>
                 </div>
             </div>
         )
