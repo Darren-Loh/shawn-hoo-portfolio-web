401815f5b69e845185bd26b60fa6e46c6a67fb5d
24kmystique <itsliwens@gmail.com>
2022-11-24 16:06:15 +0800

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className='nav-bar'>
+      <div className='nav-logo'>shawn hoo</div>
+      <ul className='nav-links'>
+        <div className='nav-menu'>
+          <li><a href='#'>about</a></li>
+          <li><a href='#'>books</a></li>
+          <li><a href='#'>publications</a></li>
+          <li><a href='#'>blog</a></li>
+          <li><a href='#'>contact</a></li>
+        </div>
+      </ul>
+      </nav>
   );
 }
 
