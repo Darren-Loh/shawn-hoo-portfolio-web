b9489dd1540aca47bb4b4ae41b866f58a6b842a5
24kmystique <itsliwens@gmail.com>
2022-11-30 18:04:50 +0800

    update contact page - alignment of items to center

--- src/Components/contact/ContactPageRight.js
+++ src/Components/contact/ContactPageRight.js
@@ -6,29 +6,29 @@ function ContactPageRight() {
 
         <div className='nameInput'>
             <div className='nameLeft'>
-                <label for="fname">first name</label>
+                <label className='contactRightLabels' for="fname" >first name</label>
                 <input type="text" id="fname" name="fname"/>
             </div>
             <div className='nameRight'>
-                <label for="lname">last name</label>
+                <label className='contactRightLabels' for="lname">last name</label>
                 <input type="text" id="lname" name="lname"/>
             </div>
         </div>
 
 
         <div className='emailInput'>
-            <label for="email">email</label>
+            <label className='contactRightLabels' for="email">email</label>
             <input type="text" id="email" name="email"/>
         </div>
 
 
         <div className='msgInput'>
-            <label for="msg">message</label>
+            <label className='contactRightLabels' for="msg">message</label>
             <textarea className='msgInputBox' type="text" id="msg" name="msg" rows="10" cols="50"/>
         </div>
 
         <button className='submitBtn' type='submit'>
-            Submit
+            SUBMIT
         </button>
 
     </form>
