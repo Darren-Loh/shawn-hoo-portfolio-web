f584744d186fb7bee31366eb47361002220a0e5a
geraldhyw <gerald5jan@yahoo.com.sg>
2022-12-05 22:14:33 +0800

    implemented function to send email for contact page

--- src/Components/contact/ContactPageRight.js
+++ src/Components/contact/ContactPageRight.js
@@ -1,38 +1,64 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 
 function ContactPageRight() {
+
+  const form = useRef();
+
+  // function to relevant information to owner via email
+  const handleSendEmail = (event) => {
+    event.preventDefault()
+
+    // retrieve relevant information
+    let firstName = event.target.fname.value
+    let lastName = event.target.lname.value
+    let senderEmail = event.target.email.value
+    let msg = event.target.msg.value
+
+    // use emailJS to send email with relevant data
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+      .then((result) => {
+          // show the user a success message
+          alert('Succesfully sent!')
+      }, (error) => {
+          // show the user an error
+          alert('Unsuccessful, please try again.')
+      });
+  }
+
   return (
-    <form className='contactPageRight'>
+    <form ref={form} className='contactPageRight' onSubmit={handleSendEmail}>
 
-        <div className='nameInput'>
-            <div className='nameLeft'>
-                <label className='contactRightLabels' htmlFor="fname" >first name</label>
-                <input type="text" id="fname" name="fname"/>
-            </div>
-            <div className='nameRight'>
-                <label className='contactRightLabels' htmlFor="lname">last name</label>
-                <input type="text" id="lname" name="lname"/>
-            </div>
+      {/* name */}
+      <div className='nameInput'>
+        <div className='nameLeft'>
+          <label className='contactRightLabels' htmlFor="fname" >first name</label>
+          <input type="text" id="fname" name="fname"/>
         </div>
-
-
-        <div className='emailInput'>
-            <label className='contactRightLabels' htmlFor="email">email</label>
-            <input type="text" id="email" name="email"/>
+        <div className='nameRight'>
+          <label className='contactRightLabels' htmlFor="lname">last name</label>
+          <input type="text" id="lname" name="lname"/>
         </div>
+      </div>
 
+      {/* email */}
+      <div className='emailInput'>
+        <label className='contactRightLabels' htmlFor="email">email</label>
+        <input type="text" id="email" name="email"/>
+      </div>
 
-        <div className='msgInput'>
-            <label className='contactRightLabels' htmlFor="msg">message</label>
-            <textarea className='msgInputBox' type="text" id="msg" name="msg" rows="10" cols="50"/>
-        </div>
+      {/* msg input */}
+      <div className='msgInput'>
+        <label className='contactRightLabels' htmlFor="msg">message</label>
+        <textarea className='msgInputBox' type="text" id="msg" name="msg" rows="10" cols="50"/>
+      </div>
 
-        <button className='submitBtn' id='contactButton' type='submit'>
-            SUBMIT
-        </button>
+      {/* submit button */}
+      <button className='submitBtn' id='contactButton' type='submit'>
+          SUBMIT
+      </button>
 
     </form>
-
   )
 }
 
