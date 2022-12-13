3b284f35e273e5640310537ba0db67304bb54f0a
Darren Loh <vanhfdarren@gmail.com>
2022-12-13 21:43:52 +0800

    book page now refers to json server

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -25,36 +25,64 @@ function Book() {
 
         return (
             
-
-
-
-
             <div>
-                <div className='main-body-top'>
-                    <div className='body-col-left'>
-                        <img className='bookcover-img' src={BookCover} />
-                    </div>
-                    <div className='body-col-right'>
-                        <h1>Of the Florids</h1>
-                        <p className='main-text publisher'>Diode Editions, 2022</p>
-                        <p className='main-text awards'>Winner of the 2021 Diode Editions Chapbook Prize</p>
-                        <p className='main-text awards'>Runner-up of the 2021 Tupelo Press Sunken Garden Chapbook Prize</p>
-                        <p className='main-text short-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus dui nec dignissim tempor. Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris. Sed sed euismod mi, sit amet hendrerit ipsum.</p>
-                        <div>
-                            <p style={{fontFamily: 'Inter'}}>Purchase a copy from:</p>
-                            <button>EPIGRAM BOOKS</button>
-                            <button style={{marginBottom: 0}}>DIODE EDITIONS</button>
+                {bookAll && bookAll.map((book) => 
+                <div>
+                    <div className='main-body-top'>
+                        <div className='body-col-left'>
+                            <img className='bookcover-img' src={BookCover} />
+                        </div>
+                        <div className='body-col-right'>
+                            <h1>{book.title}</h1>
+                            <p className='main-text publisher'>{book.edition}</p>
+                            <p className='main-text awards'>{book.awards}</p>
+                            <p className='main-text short-text'>{book.description}</p>
+                            <div>
+                                <p style={{fontFamily: 'Inter'}}>Purchase a copy from:</p>
+                                <button>EPIGRAM BOOKS</button>
+                                <button style={{marginBottom: 0}}>DIODE EDITIONS</button>
+                            </div>
                         </div>
                     </div>
+                    <div>
+                        <h2 className='h2-header'>Reviews</h2>
+                        {book.reviews.map((review) => <p className='text'><span>“</span><span>{review[0]}</span><span>” —</span><span className='reviews-signoff-name'>{review[1]}</span></p> )}
+                        
+                        <h2 className='h2-header'>Interviews</h2>
+                        <h2 className='h2-header'>Description</h2>
+                    </div>
                 </div>
-                <div>
-                    <h2 className='h2-header'>Reviews</h2>
-                    <p className='text'><span>“Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris.” —</span><span className='reviews-signoff-name'>John Doe</span></p>
-                    <p className='text'><span>“Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris.” —</span><span className='reviews-signoff-name'>John Doe</span></p>
-                    <h2 className='h2-header'>Interviews</h2>
-                    <h2 className='h2-header'>Description</h2>
-                </div>
+                )}
             </div>
+
+
+
+            // <div>
+            //     <div className='main-body-top'>
+            //         <div className='body-col-left'>
+            //             <img className='bookcover-img' src={BookCover} />
+            //         </div>
+            //         <div className='body-col-right'>
+            //             <h1>Of the Florids</h1>
+            //             <p className='main-text publisher'>Diode Editions, 2022</p>
+            //             <p className='main-text awards'>Winner of the 2021 Diode Editions Chapbook Prize</p>
+            //             <p className='main-text awards'>Runner-up of the 2021 Tupelo Press Sunken Garden Chapbook Prize</p>
+            //             <p className='main-text short-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus dui nec dignissim tempor. Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris. Sed sed euismod mi, sit amet hendrerit ipsum.</p>
+            //             <div>
+            //                 <p style={{fontFamily: 'Inter'}}>Purchase a copy from:</p>
+            //                 <button>EPIGRAM BOOKS</button>
+            //                 <button style={{marginBottom: 0}}>DIODE EDITIONS</button>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     <div>
+            //         <h2 className='h2-header'>Reviews</h2>
+            //         <p className='text'><span>“Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris.” —</span><span className='reviews-signoff-name'>John Doe</span></p>
+            //         <p className='text'><span>“Vivamus viverra dictum libero, sed tempus arcu varius id. Nam luctus luctus enim, sit amet viverra arcu consequat fringilla. Sed non lectus eu elit convallis finibus in vitae orci. In vel turpis mollis, pharetra ex quis, ultrices mauris.” —</span><span className='reviews-signoff-name'>John Doe</span></p>
+            //         <h2 className='h2-header'>Interviews</h2>
+            //         <h2 className='h2-header'>Description</h2>
+            //     </div>
+            // </div>
         )
     
 }
