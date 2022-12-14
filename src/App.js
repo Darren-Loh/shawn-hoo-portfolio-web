c20aa12c86ba08e2d8cfea2d4c8b6097818ae5cb
24kmystique <itsliwens@gmail.com>
2022-12-14 13:21:05 +0800

    modularised nav menu component into 2 components - MediumNavMenu and SmallNavMenu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import {
   BrowserRouter,
   Routes,
-  Route,
-  NavLink,
+  Route
 } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 import './App.css';
 import About from './Components/About.js';
@@ -17,69 +17,20 @@ import AboutAdmin from "./Components/adminComponents/about_Admin_folder/AboutAdm
 import ContactAdmin from "./Components/adminComponents/contact_Admin_folder/ContactAdmin";
 import PublicationsPageAdmin from "./Components/adminComponents/publications_Admin_folder/PublicationsPageAdmin";
 import ProtectedLayout from './Components/ProtectedLayout';
+import MediumNavMenu from "./Components/navMenu/mediumNavMenu";
+import SmallNavMenu from "./Components/navMenu/smallNavMenu";
 
-import { useState, useEffect } from 'react'
-import { motion } from "framer-motion"
+
 
 function App() {
 
-  const windowSize = useWindowSize()
-  const viewWidth = windowSize.width
-
-  const [navClicked, setNavClicked] = useState(false)
-
-  const navVariant = {
-    hidden: {
-      x:-1000,
-      transition: { ease: "easeOut", duration: 0.8 }
-    },
-    visible: {
-      x:0,
-      transition: { ease: "easeIn", duration: 0.8 }
-    }
-  }
+  const windowSize = useWindowSize();
+  const viewWidth = windowSize.width;
 
   return (
     <BrowserRouter>
       <div className="App">
-        {(viewWidth > 480) ? (
-          <nav className='nav-bar'>
-          <div className='nav-logo'>
-            <NavLink to='/' className="logo">shawn hoo</NavLink>
-          </div>
-          <ul className='nav-links'>
-            <div className='nav-menu'>
-              <li><NavLink to='/about' className={({isActive}) => isActive ? "active" : undefined}>about</NavLink></li>
-              <li><NavLink to='/of-the-florids' className={({isActive}) => isActive ? "active" : undefined}>of the florids</NavLink></li>
-              <li><NavLink to='/publications' className={({isActive}) => isActive ? "active" : undefined}>publications</NavLink></li>
-              <li><NavLink to='/blog' className={({isActive}) => isActive ? "active" : undefined}>blog</NavLink></li>
-              <li><NavLink to='/contact' className={({isActive}) => isActive ? "active" : undefined}>contact</NavLink></li>
-            </div>
-          </ul>
-        </nav>
-        ) : (
-          <nav className='nav-bar-small'>
-            <div className="nav-bar-main-small" onClick={() => {setNavClicked(!navClicked)}}>
-              <NavLink to='/' className="nav-bar-small-logo">shawn hoo</NavLink>
-            </div>
-
-            <ul className='nav-links-small'>
-              <motion.div 
-              className='nav-menu-small'
-              variants={navVariant}
-              initial="hidden"
-              animate={navClicked ? "visible" : "hidden"}
-              >
-                <li><NavLink to='/about' className={({isActive}) => isActive ? "active" : undefined}>about</NavLink></li>
-                <li><NavLink to='/of-the-florids' className={({isActive}) => isActive ? "active" : undefined}>of the florids</NavLink></li>
-                <li><NavLink to='/publications' className={({isActive}) => isActive ? "active" : undefined}>publications</NavLink></li>
-                <li><NavLink to='/blog' className={({isActive}) => isActive ? "active" : undefined}>blog</NavLink></li>
-                <li><NavLink to='/contact' className={({isActive}) => isActive ? "active" : undefined}>contact</NavLink></li>
-              </motion.div>
-            </ul>
-          </nav>
-        )}
-        
+        {(viewWidth > 480) ? <MediumNavMenu /> : <SmallNavMenu /> }
         
         <Routes>
           <Route path='/' element={<HomePage />} />
@@ -111,6 +62,7 @@ function useWindowSize() {
     width: undefined,
     height: undefined,
   });
+
   useEffect(() => {
     // Handler to call on window resize
     function handleResize() {
@@ -119,7 +71,8 @@ function useWindowSize() {
         width: window.innerWidth,
         height: window.innerHeight,
       });
-    }
+    };
+
     // Add event listener
     window.addEventListener("resize", handleResize);
     // Call handler right away so state gets updated with initial window size
@@ -127,6 +80,7 @@ function useWindowSize() {
     // Remove event listener on cleanup
     return () => window.removeEventListener("resize", handleResize);
   }, []); // Empty array ensures that effect is only run on mount
+
   return windowSize;
 }
 
