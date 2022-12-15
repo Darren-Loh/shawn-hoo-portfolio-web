00205af4787c704296b62583c7dffb52c84dca62
geraldhyw <gerald5jan@yahoo.com.sg>
2022-12-15 12:18:42 +0800

    fixed header interactions

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ import ProtectedLayout from './Components/ProtectedLayout';
 
 import { useState, useEffect } from 'react'
 import { motion } from "framer-motion"
+import { FaBars } from "react-icons/fa";
+
 
 function App() {
 
@@ -30,11 +32,13 @@ function App() {
 
   const navVariant = {
     hidden: {
-      x:-1000,
+      x:-600,
+      opacity: 0,
       transition: { ease: "easeOut", duration: 0.8 }
     },
     visible: {
       x:0,
+      opacity: 1,
       transition: { ease: "easeIn", duration: 0.8 }
     }
   }
@@ -59,24 +63,24 @@ function App() {
         </nav>
         ) : (
           <nav className='nav-bar-small'>
-            <div className="nav-bar-main-small" onClick={() => {setNavClicked(!navClicked)}}>
-              <NavLink to='/' className="nav-bar-small-logo">shawn hoo</NavLink>
+            <div className="nav-bar-main-small">
+              <FaBars className="nav-bar-small-icon" onClick={() => {setNavClicked(!navClicked)}}/>
+              <NavLink to='/' className="nav-bar-small-logo" onClick={() => {setNavClicked(false)}}>shawn hoo</NavLink>
             </div>
 
-            <ul className='nav-links-small'>
-              <motion.div 
+            <motion.div 
+              // className='nav-links-small'
               className='nav-menu-small'
               variants={navVariant}
               initial="hidden"
               animate={navClicked ? "visible" : "hidden"}
-              >
-                <li><NavLink to='/about' className={({isActive}) => isActive ? "active" : undefined}>about</NavLink></li>
-                <li><NavLink to='/of-the-florids' className={({isActive}) => isActive ? "active" : undefined}>of the florids</NavLink></li>
-                <li><NavLink to='/publications' className={({isActive}) => isActive ? "active" : undefined}>publications</NavLink></li>
-                <li><NavLink to='/blog' className={({isActive}) => isActive ? "active" : undefined}>blog</NavLink></li>
-                <li><NavLink to='/contact' className={({isActive}) => isActive ? "active" : undefined}>contact</NavLink></li>
-              </motion.div>
-            </ul>
+              onClick={() => {setNavClicked(false)}}>
+              <li><NavLink to='/about' className={({isActive}) => isActive ? "active" : undefined}>about</NavLink></li>
+              <li><NavLink to='/of-the-florids' className={({isActive}) => isActive ? "active" : undefined}>of the florids</NavLink></li>
+              <li><NavLink to='/publications' className={({isActive}) => isActive ? "active" : undefined}>publications</NavLink></li>
+              <li><NavLink to='/blog' className={({isActive}) => isActive ? "active" : undefined}>blog</NavLink></li>
+              <li><NavLink to='/contact' className={({isActive}) => isActive ? "active" : undefined}>contact</NavLink></li>
+            </motion.div>
           </nav>
         )}
         
