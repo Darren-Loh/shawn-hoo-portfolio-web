b38ceadcedb1dbe56683da169c8f2adc4d35a02c
24kmystique <itsliwens@gmail.com>
2022-12-28 16:40:31 +0800

    removed hardcoded login redirect uri

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
       <Auth0Provider
         domain={process.env.REACT_APP_AUTH0_DOMAIN}
         clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-        redirectUri={"http://localhost:3000/admin"}
+        redirectUri={process.env.REACT_APP_AUTH0_CALLBACK_URL}
       >
         <App />
       </Auth0Provider>
